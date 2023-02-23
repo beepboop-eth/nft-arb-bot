2cf3730ee55c6d31ca2df169aeb9d8cf45c40191
Janus Manlapaz <alphaomega030894@gmail.com>
2023-02-23 11:06:20 -0500

    blur.js

---
+++ blur.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    userDataDir: '/Users/janusmanlapaz/Library/Application Support/Google/Chrome/',
+    args: [
+      '--disable-features=IsolateOrigins,site-per-process',
+      `--profile-directory=Profile 12`,
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage'
+    ],
+    executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  });
+
+  const page = await browser.newPage();
+  const url = 'https://core-api.prod.blur.io/v1/collections/boredapeyachtclub/executable-bids?filters=%7B%7D';
+  await page.goto(url);
+
+  const data = await page.content();
+  fs.writeFileSync('data.json', data);
+
+  await browser.close();
+})();
