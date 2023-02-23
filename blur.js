4ba9b2370059944cdb60dff36da10b6c98899ecb
Janus Manlapaz <alphaomega030894@gmail.com>
2023-02-23 12:19:35 -0500

    it's over

--- blur.js
+++ blur.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 
 (async () => {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
     userDataDir: '/Users/janusmanlapaz/Library/Application Support/Google/Chrome/',
     args: [
       '--disable-features=IsolateOrigins,site-per-process',
@@ -13,14 +13,32 @@ const fs = require('fs');
       '--disable-dev-shm-usage'
     ],
     executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+  }).catch((err) => {
+    console.log('err', err)
   });
 
   const page = await browser.newPage();
+
   const url = 'https://core-api.prod.blur.io/v1/collections/boredapeyachtclub/executable-bids?filters=%7B%7D';
+  await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36');
   await page.goto(url);
 
-  const data = await page.content();
-  fs.writeFileSync('data.json', data);
+  // Get the JSON data from the page content
+  const jsonData = await page.evaluate(() => {
+    const pre = document.querySelector('pre');
+    if (pre) {
+      return JSON.parse(pre.textContent);
+    }
+    return null;
+  });
+
+  if (jsonData) {
+    // Save the data to a file
+    fs.writeFileSync('data.json', JSON.stringify(jsonData));
+    console.log('Data saved to data.json');
+  } else {
+    console.log('JSON data not found in the page content.');
+  }
 
   await browser.close();
 })();
